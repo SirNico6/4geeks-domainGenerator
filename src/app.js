022b8fb8c6822fbafe5b3170961d13080fd56469
Nicolas Arevalo <gabrielnicolas512@outlook.com>
2024-09-18 19:48:04 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+let pronounExamples = ['the', 'our'];
+let adjExamples     = ['great', 'big'];
+let nounExamples    = ['jogger', 'racoon', 'puedes'];
+let extExamples     = ['org', 'com', 'er', 'es']
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let generateRandomDomains = () => {
+  pronounExamples.forEach((prenoun) => {
+    adjExamples.forEach((adj) => {
+      nounExamples.forEach((noun) => {
+        extExamples.forEach((ext) => {
+          if (noun.endsWith(ext)) {
+            console.log(`${prenoun}${adj}${noun.slice(0, -ext.length)}.${ext}`);
+          } else{ 
+            console.log(`${prenoun}${adj}${noun}.${ext}`);
+          }
+        });
+      });
+    });
+  });
+}
+
+generateRandomDomains()
